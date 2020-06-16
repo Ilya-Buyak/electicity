f712c99eab22d7acf5e2360bb5e9327c72235c83
Ilya <Ilya_11_11@mail.ru>
2020-06-16 20:29:07 +0300

    config fix

---
+++ components/blocks/Container.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.container {
+  max-width: 1440px;
+  padding: 0 15px;
+}
+</style>
---
+++ components/ui/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button :class="['btn', `btn_theme_${theme}`, { btn_disabled: disabled }]">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    theme: String,
+    disabled: Boolean,
+  },
+};
+</script>
+
+<style scoped></style>
